97ab1b89d4652e1ae67e7c8ee6e6589f8d877ea7
Rafael Martins Herculano <rafael.herculano@bbtecno.com.br>
2019-11-22 16:56:05 -0300

    Implementando upload-avatar

--- index.js
+++ index.js
@@ -2,11 +2,52 @@ const express = require('express');
 // var docxConverter = require('docx-pdf');
 
 const app = express();
+const PORT = process.env.PORT || 3000 ;
 
-const PORT = process.env.PORT || 3000  ;
+app.use(express.static('resources'));
+
+// enable files upload
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+//add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send('Hello Test')
+});
+
+app.post('/upload-avatar', async (req, res) => {
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+            let avatar = req.files.avatar;
+            
+            //Use the mv() method to place the file in upload directory (i.e. "uploads")
+            avatar.mv('./resources/' + avatar.name);
+
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: avatar.name,
+                    mimetype: avatar.mimetype,
+                    size: avatar.size
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 app.listen(PORT, () => {
