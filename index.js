188262498a322a060100bd0e04dd556c9db02f8b
Rafael Martins Herculano <rafael.herculano@bbtecno.com.br>
2019-11-25 11:51:25 -0300

    Corrigindo rota 2

--- index.js
+++ index.js
@@ -32,52 +32,7 @@ app.post('/upload-avatar', async (req, res) => {
 });
 
 app.post('/upload-files', async (req, res) => {
-    try {
-        if(!req.files) {
-            res.send({
-                status: false,
-                message: 'No file uploaded'
-            });
-        } else {
-            let data = []; 
-    
-            //loop all files
-            _.forEach(_.keysIn(req.files.photos), (key) => {
-                let photo = req.files.photos[key];
-                
-                //move photo to uploads directory
-                photo.mv('./uploads/' + photo.name);
-
-                //push file details
-                data.push({
-                    name: photo.name,
-                    mimetype: photo.mimetype,
-                    size: photo.size
-                });
-            });
-
-            // merge(source_files,dest_file_path,function(err){
-            //     if(err) {
-            //       return console.log(err)
-            //     }
-            //     console.log('Success')
-            //   });
-
-            const d = data.map(d => {
-                return { fileUrl: path.resolve(__dirname, d.name) } ;
-            })
-            
-            //return response
-            res.send({
-                status: true,
-                message: 'Files are uploaded',
-                data: data,
-                filesUrls: d
-            });
-        }
-    } catch (err) {
-        res.status(500).send(err);
-    }
+    res.send(req.body.files);
 });
 
 
