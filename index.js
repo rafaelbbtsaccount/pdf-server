e7512b8ab57e7f1923aaadce5f54f3f6e22bd7f5
Rafael Martins Herculano <rafael.herculano@bbtecno.com.br>
2019-11-25 11:37:48 -0300

    Corrigindo rota

--- index.js
+++ index.js
@@ -27,33 +27,8 @@ app.get('/', (req, res) => {
 });
 
 app.post('/upload-avatar', async (req, res) => {
-    try {
-        if(!req.files) {
-            res.send({
-                status: false,
-                message: 'No file uploaded'
-            });
-        } else {
-            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-            let avatar = req.files.avatar;
-            
-            //Use the mv() method to place the file in upload directory (i.e. "uploads")
-            avatar.mv('./resources/' + avatar.name);
-
-            //send response
-            res.send({
-                status: true,
-                message: 'File is uploaded',
-                data: {
-                    name: avatar.name,
-                    mimetype: avatar.mimetype,
-                    size: avatar.size
-                }
-            });
-        }
-    } catch (err) {
-        res.status(500).send(err);
-    }
+    
+  res.send(req.body.files);
 });
 
 app.post('/upload-files', async (req, res) => {
